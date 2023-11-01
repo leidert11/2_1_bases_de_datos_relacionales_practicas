1c335a160f1dc2a328d6aa41826f0a067287e76c
leidert11 <110786757+leidert11@users.noreply.github.com>
2023-10-31 22:40:57 -0500

    fix: :card_file_box: corregir errores en las tablas de la base de datos

--- biblioteca.sql
+++ biblioteca.sql
@@ -7,19 +7,18 @@ create table libros (
     isbn int 
 );
 
-alter table libros modify isbn INT AUTO_INCREMENT not null ,
+alter table libros modify isbn INT AUTO_INCREMENT not null,
 add CONSTRAINT PRIMARY KEY (isbn);
 
 alter table libros add titulo varchar(255) not null;
 alter table libros add age int not null;
 alter table libros add precio float not null;
 
-
 create table autores (
     id int
 );
 
-alter table autores modify id INT AUTO_INCREMENT not null ,
+alter table autores modify id INT AUTO_INCREMENT not null,
 add CONSTRAINT PRIMARY KEY (id);
 
 alter table autores add nombre varchar(255) not null;
@@ -29,7 +28,7 @@ create table editores (
     id int
 );
 
-alter table editores modify id INT AUTO_INCREMENT not null ,
+alter table editores modify id INT AUTO_INCREMENT not null,
 add CONSTRAINT PRIMARY KEY (id);
 
 alter table editores add nombre varchar(255) not null;
@@ -40,7 +39,7 @@ create table almacenes (
     id int
 );
 
-alter table almacenes modify id INT AUTO_INCREMENT not null ,
+alter table almacenes modify id INT AUTO_INCREMENT not null,
 add CONSTRAINT PRIMARY KEY (id);
 
 alter table almacenes add nombre varchar(255) not null;
@@ -48,11 +47,11 @@ alter table almacenes add direccion varchar(255) not null;
 alter table almacenes add numero_telefonico int not null;
 
 CREATE table libro_in_almacen (
+id_libro int not null,
+id_almacen int not null,
+cantidad_libro int not null
 );
 
-alter table libro_in_almacen add id_libro int not null;
-alter table libro_in_almacen add id_almacen int not null;
-alter table libro_in_almacen add cantidad_libro int not null;
 
 create table clientes (
     id int
@@ -62,7 +61,7 @@ alter table clientes modify id int AUTO_INCREMENT not null,
 add CONSTRAINT PRIMARY KEY (id);
 
 alter table clientes add nombre varchar(255) not null;
-alter table clientes add correco varchar(100) not null UNIQUE,
+alter table clientes add correo varchar(100) not null UNIQUE
 alter table clientes add direccion varchar(255) not null UNIQUE;
 alter table clientes add ciudad varchar(255) not null;
 alter table clientes add numero_telefonico int not null;
@@ -92,54 +91,35 @@ alter table orden add fecha_orden date not null;
 alter table orden add direccion_de_envio varchar(255) not null;
 alter table orden add informacion_de_pago varchar(255) not null;
 
-
 CREATE TABLE autores_libros (
+    id_autores INT NOT NULL ,
+    id_libro INT NOT NULL 
 );
-
-alter table autores_libros add id_autores  int not null;
-alter table autores_libros add id_libro  int not null;
-
 
 CREATE TABLE editores_libros (
+    id_libro INT NOT NULL ,
+    id_editor INT NOT NULL 
 );
-
-alter table editores_libros add id_libro  int not null;
-alter table editores_libros add id_editor  int not null;
 
 CREATE TABLE cliente_orden (
+    id_cliente INT NOT NULL ,
+    id_orden INT NOT NULL 
 );
-
-
-alter table cliente_orden add id_cliente  int not null;
-alter table cliente_orden add id_orden  int not null;
 
 CREATE TABLE carrito_orden (
+    id_orden INT NOT NULL ,
+    id_carrito INT NOT NULL 
 );
 
-alter table carrito_orden add id_orden  int not null;
-alter table carrito_orden add id_carrito  int not null;
-
-
 ALTER TABLE libro_in_almacen ADD CONSTRAINT libro_in_almacen_fk0 FOREIGN KEY (id_libro) REFERENCES libros(isbn);
-
 ALTER TABLE libro_in_almacen ADD CONSTRAINT libro_in_almacen_fk1 FOREIGN KEY (id_almacen) REFERENCES almacenes(id);
-
 ALTER TABLE carrito_compras ADD CONSTRAINT carrito_compras_fk0 FOREIGN KEY (id_cliente) REFERENCES clientes(id);
-
 ALTER TABLE carrito_compras ADD CONSTRAINT carrito_compras_fk1 FOREIGN KEY (id_libro) REFERENCES libros(isbn);
-
 ALTER TABLE autores_libros ADD CONSTRAINT autores_libros_fk0 FOREIGN KEY (id_autores) REFERENCES autores(id);
-
 ALTER TABLE autores_libros ADD CONSTRAINT autores_libros_fk1 FOREIGN KEY (id_libro) REFERENCES libros(isbn);
-
 ALTER TABLE editores_libros ADD CONSTRAINT editores_libros_fk0 FOREIGN KEY (id_libro) REFERENCES libros(isbn);
-
 ALTER TABLE editores_libros ADD CONSTRAINT editores_libros_fk1 FOREIGN KEY (id_editor) REFERENCES editores(id);
-
 ALTER TABLE cliente_orden ADD CONSTRAINT cliente_orden_fk0 FOREIGN KEY (id_cliente) REFERENCES clientes(id);
-
 ALTER TABLE cliente_orden ADD CONSTRAINT cliente_orden_fk1 FOREIGN KEY (id_orden) REFERENCES orden(id);
-
 ALTER TABLE carrito_orden ADD CONSTRAINT carrito_orden_fk0 FOREIGN KEY (id_orden) REFERENCES orden(id);
-
 ALTER TABLE carrito_orden ADD CONSTRAINT carrito_orden_fk1 FOREIGN KEY (id_carrito) REFERENCES carrito_compras(id);
